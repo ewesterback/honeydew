8647fd051555da57f2baaf1fa4f9bba659484756
Emily Westerback <emilywesterback2@gmail.com>
2021-07-08 16:34:37 -0700

    played around with login

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import './styles/App.css'
 import { React, useEffect } from 'react'
-import {Route, Switch, useHistory} from 'react-router-dom'
+import { Route, Switch, useHistory } from 'react-router-dom'
 import { connect } from 'react-redux'
 import {
   LoadItems,
@@ -10,10 +10,7 @@ import {
   ToggleComplete
 } from './store/actions/ItemActions'
 import {} from './store/actions/ListActions'
-import {
-  LoadUser,
-  SetAuth
-} from './store/actions/UserActions'
+import { LoadUser, SetAuth } from './store/actions/UserActions'
 import Login from './pages/Login'
 import Register from './pages/Register'
 import Dashboard from './pages/Dashboard'
@@ -25,60 +22,64 @@ const mapStateToProps = ({ itemState, listState, userState }) => {
 const mapDispatchToProps = (dispatch) => {
   return {
     // ITEMS
-    fetchItems: (listID)=> dispatch(LoadItems(listID)),
+    fetchItems: (listID) => dispatch(LoadItems(listID)),
     stageItem: (value) => dispatch(StageItem(value)),
-    addItem: (item)=> dispatch(AddItem(item)),
-    removeItem: (itemID)=> dispatch(RemoveItem(itemID)),
-    toggleComplete: (itemID,status) => dispatch(ToggleComplete(itemID,status)),
+    addItem: (item) => dispatch(AddItem(item)),
+    removeItem: (itemID) => dispatch(RemoveItem(itemID)),
+    toggleComplete: (itemID, status) =>
+      dispatch(ToggleComplete(itemID, status)),
     // LISTS
     // USER
-    fetchUser: (loginEmail,loginPassword) => dispatch(LoadUser(loginEmail,loginPassword)),
-    setAuth: (bool) => dispatch(SetAuth(bool)),
+    fetchUser: (loginEmail, loginPassword) =>
+      dispatch(LoadUser(loginEmail, loginPassword)),
+    setAuth: (bool) => dispatch(SetAuth(bool))
   }
 }
-
 
 function App(props) {
   const history = useHistory()
   const { userState, itemState, listState } = props
 
-    //// AUTHENTICATION
+  //// AUTHENTICATION
 
-
-    const logOut = () => {
-      props.setAuth(false)
-      localStorage.clear()
-      history.push('/')
+  const logOut = () => {
+    props.setAuth(false)
+    localStorage.clear()
+    history.push('/')
+  }
+  const getToken = () => {
+    let token = localStorage.getItem('token')
+    console.log(token)
+    if (token) {
+      //return props.setAuth(true)
     }
-    const getToken = () => {
-      let token = localStorage.getItem('token')
-      if (token) {
-        return props.setAuth(true)
-      }
-    }
+  }
   useEffect(() => {
     getToken()
   }, [])
 
-  
   return (
     <Switch>
       <Route
         path="/login"
         component={(props) => (
-          <Login {...props}
-          userState = {userState} />
+          <Login {...props} userState={userState} fetchUser={props.fetchUser} />
         )}
       />
       <Route
-        exact path="/"
-        component={(props)=>(
-          <Dashboard {...props} />
+        exact
+        path="/"
+        component={(props) => (
+          <Dashboard
+            {...props}
+            userState={userState}
+            fetchUser={props.fetchUser}
+          />
         )}
       />
-      <Register/>
+      <Register />
     </Switch>
-  );
+  )
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(App)
