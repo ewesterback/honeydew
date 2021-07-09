c8d40c78ca2be91faa09acece2e442078fd4bd73
Lindsey Moss <llmodbls@gmail.com>
2021-07-09 15:23:05 -0400

    styled todo form

--- client/src/components/TodoForm.js
+++ client/src/components/TodoForm.js
@@ -12,27 +12,36 @@ import {
 const TodoForm = (props) => {
   return (
     <div className="todo-form">
-      <Card>
+      <Card
+        style={{
+          width: '70%',
+          padding: '1.5em 1em',
+          fontFamily: 'Overlock'
+        }}>
         <div className="form-card">
-          <DatePicker
-            minDate={new Date()}
-            placeholder="Due Date"
-            onChange={props.handleDateChange}
-            value={props.itemState.newItem.due_date}
-            className="form-item"
-            id="form-date-picker"
-          />
           <Input
-            placeholder="title"
+            placeholder="To-do Title"
             value={props.itemState.newItem.title}
             onChange={props.handleTitleChange}
             className="form-item"
+            style={{
+              marginBottom:'1em',
+              width: '60%',
+              marginRight: '5%'
+            }}
           />
-          <CheckboxToggle
-            value={props.itemState.newItem.priority}
-            onChange={props.handlePriorityChange}
-            label="High Priority?"
-          />
+            <DatePicker
+              minDate={new Date()}
+              placeholder="Due Date"
+              onChange={props.handleDateChange}
+              value={props.itemState.newItem.due_date}
+              className="form-item"
+              id="form-date-picker"
+              style={{
+                marginBottom:'1em',
+                width: '35%'
+              }}
+            />
           {/* <Input
             placeholder="priority"
             value={props.itemState.newItem.priority}
@@ -40,23 +49,43 @@ const TodoForm = (props) => {
             className="form-item"
           /> */}
           <Textarea
-            placeholder="content"
+            placeholder="Details"
             value={props.itemState.newItem.content}
             onChange={props.handleContentChange}
             className="form-item"
+            style={{
+              marginBottom:'1em',
+              width: '100%',
+              fontFamily: 'Overlock'
+            }}
           />
+            <CheckboxToggle
+              value={props.itemState.newItem.priority}
+              onChange={props.handlePriorityChange}
+              label="High Priority?"
+            />
+          <div className="form-button-box">
           <Button
             variant="destructive"
             onClick={props.toggleNewTodoForm}
             label="Cancel"
             className="form-item"
+            style={{
+              fontFamily: 'Righteous',
+              letterSpacing: '0.03em'
+            }}
           />
           <Button
             variant="success"
             onClick={props.submitTodo}
             label="Submit To-do"
             className="form-item"
+            style={{
+              fontFamily: 'Righteous',
+              letterSpacing: '0.03em'
+            }}
           />
+          </div>
         </div>
       </Card>
     </div>
